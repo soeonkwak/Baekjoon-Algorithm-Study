d3c7ae76b030cb03a0b5e90ce03995cea1482f8b
Soeon <rhkrthdjs@naver.com>
2022-09-05 02:53:23 +0900

    master branchA branchB merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello, I'm branchA !");
         System.out.println("Hello, I'm branchB!");
+        System.out.println("master branchA + branchB merge");
     }
 }
