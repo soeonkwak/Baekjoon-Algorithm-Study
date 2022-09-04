2bd69f6ac21d5190b020d4ee204edeb5e955fbe1
Soeon <rhkrthdjs@naver.com>
2022-09-05 01:33:07 +0900

    branch0905 commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("add GitBashTest branch line!");
         System.out.println("add branch0905 new line!");
+        System.out.println("add branch0905 new line2222!");
     }
 }
